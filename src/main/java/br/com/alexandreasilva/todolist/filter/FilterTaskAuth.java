f9030a219c5f6be3ac091f4c8e40692f25da461c
unknown <alexandre8603@gmail.com>
2023-10-16 23:17:06 -0300

    get headers auth

--- src/main/java/br/com/alexandreasilva/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/alexandreasilva/todolist/filter/FilterTaskAuth.java
@@ -3,23 +3,29 @@ package br.com.alexandreasilva.todolist.filter;
 import java.io.IOException;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        
-         System.out.println("Chegou no filtro.");       
-        chain.doFilter(request, response);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+             
+            var auth =  request.getHeader("Authorization") ; 
+            System.out.println(auth);
+
+
+            filterChain.doFilter(request, response);
     }
-    
-    
+
+
 }
+    
+    
